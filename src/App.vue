712d3b80a6ca3c6622f9080a8a30e8880e210516
MatheusGSR <matheusnely@hotmail.com>
2023-09-26 07:43:37 -0300

    .

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello World'
+    };
+  }
+};
+
+
+</script>
+
+<style>
+</style>
